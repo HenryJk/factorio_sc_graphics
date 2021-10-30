cda187435c153e3cd203a461686fe1a45d600765
Henry Jayakusuma <hsmgj2010@gmail.com>
2021-10-30 14:28:14 +0800

    update submodule

--- build.rs
+++ build.rs
@@ -1,4 +1,5 @@
 extern crate cmake;
+
 use cmake::Config;
 
 fn main() {
@@ -9,5 +10,6 @@ fn main() {
 
     println!("cargo:rustc-link-search=native={}/lib", dst.display());
     println!("cargo:rustc-link-lib=static=casc");
-    println!("cargo:rustc-link-lib=dylib=stdc++");
+    println!("cargo:rustc-link-lib=dylib=stdc++");                    // stdc++
+    println!("cargo:rustc-link-lib=dylib=z");                         // zlib
 }
